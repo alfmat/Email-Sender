f052d5fdb172db71e9a8f3d4dd39064164b127e5
alfmat <alfredmathew@outlook.com>
2018-12-26 15:08:57 -0500

    added first file to html folder, changed process for html file import in python code

--- sender.py
+++ sender.py
@@ -16,7 +16,7 @@ def send_email():
     sender_email = "alfredmathew718@gmail.com"
     receiver_email = "alfredmathew@outlook.com"  # Enter receiver address
     smtp_server = "smtp.gmail.com"
-    message = MIMEMultipart('alternative')
+    message = MIMEMultipart()
     message['Subject'] = 'Multipart Test'
     message['From'] = sender_email  # Enter your address
     message['To'] = receiver_email
@@ -25,30 +25,26 @@ def send_email():
     password = input("Type your password and press enter: ")
 
     # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = """\
-    <html>
-    <body>
-        <p>Hi,<br>
-        How are you?<br>
-        <a href="http://www.realpython.com">Real Python</a> 
-        has many great tutorials.
-        </p>
-    </body>
-    </html>
-    """
+    # text = '''\
+    # Hi,
+    # How are you?
+    # Real Python has many great tutorials:
+    # www.realpython.com'''
+    html_file = open('./html/sample.html','rt')
+    html='''\
+    '''
+    for line in html_file:
+        html+=f'{line}\n'
+    html_file.close()
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
+    # part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    message.attach(part1)
+    # message.attach(part1)
     message.attach(part2)
+
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
